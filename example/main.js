7b84925f0f70f6b8355a41efb9c8c2c319d7349a
Thomas Bruun <thomas.bruun@gmail.com>
2013-12-02 16:45:47 +0100

    Show file type and size after drop

--- example/main.js
+++ example/main.js
@@ -2,18 +2,17 @@ var myApp = angular.module('myApp', []);
 myApp.controller('dragAndDropController', function($scope) {
 
     $scope.showUploadBox = false;
-    $scope.handleDragEvent = function() {
+    $scope.handleDragEvent = function(event) {
         $scope.showUploadBox = true;
-        console.log("Drag!");
     };
 
     $scope.handleDroppedFile = function(files) {
         // Do whatever you please with the dropped file(s)
-        if (!files) {
+        if (!files || files.length === 0) {
             $scope.showUploadBox = false;
-            console.log("Drag left or nothing dropped");
             return;
         }
-        console.log("Drop!");
+        $scope.fileSize = files[0].size;
+        $scope.fileType = files[0].type;
     };
 });
